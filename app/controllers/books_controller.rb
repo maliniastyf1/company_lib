74e42e8ae49a61ac09f82bdba94bab1c3cfebd47
maliniastyf1 <malinowski.adam85@gmail.com>
2016-12-30 00:19:51 +0100

    Implemented creation process for books and refactored

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,33 @@
 class BooksController < ApplicationController
-
+  before_action :set_book, only: [:show]
   def index
-
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to @book, notice: "Your book was added successfully"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:author, :title, :description)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
 end
--- spec/features/book_spec.rb
+++ spec/features/book_spec.rb
@@ -11,4 +11,23 @@
         expect(page).to have_content(/Books/)
       end
     end
+
+    describe 'creation' do
+      before do
+        visit new_book_path
+      end
+      it 'has a new form that can be reached' do
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'can be created from new form page' do
+        fill_in 'book[author]', with: "Adam"
+        fill_in 'book[title]', with: "Lord"
+        fill_in 'book[description]', with: "Lord is fantasy book"
+
+        click_on "Save"
+
+        expect(page).to have_content("Lord is fantasy book")
+      end
+    end
   end
