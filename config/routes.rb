a8296d71265b3223156c6987a151c3099c1c5d10
maliniastyf1 <malinowski.adam85@gmail.com>
2016-12-31 20:37:56 +0100

    Add omniauth gem and setup with devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :books
   resources :rents, only: [:new, :create, :index]
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'static#homepage'
 
 end
