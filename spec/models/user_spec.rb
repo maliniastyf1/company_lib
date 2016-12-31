4e28ab59080a88044d3af345d193668df2f8cbea
maliniastyf1 <malinowski.adam85@gmail.com>
2016-12-31 12:36:40 +0100

    Update user model and devise registration view

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,22 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
-    @user = User.create(email: "adam@test.com", password: "qwerty", password_confirmation: "qwerty", first_name: "Adam", last_name: "Malyn")
+    @user = User.create(email: "adam@test.com", password: "qwerty", password_confirmation: "qwerty")
   end
 
   describe "creation" do
     it "can be created" do
       expect(@user).to  be_valid
     end
-
-    it "cannot be created without first_name, last_name" do
-      @user.first_name = nil
-      @user.last_name = nil
-      expect(@user).to_not be_valid
-    end
   end
-
-
 end
 
 
